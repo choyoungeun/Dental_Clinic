60e5b79026f4079f86547abcc9551b00d355ba3a
Young-eun Cho <45972095+choyoungeun@users.noreply.github.com>
2026-09-23 00:06:27 +0900

    layout.tsx 업데이트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/images/yonsei.jpg",
+        url: "/images/yonsei.png",
         width: 1200,
         height: 630,
         alt: "수원세브란스치과",
@@ -48,7 +48,7 @@ export const metadata: Metadata = {
     title: "수원세브란스치과",
     description:
       "수원 장안구 송죽동 치과 · 자연치아 보존 · 임플란트 · 구강외과",
-    images: ["/images/yonsei.jpg"],
+    images: ["/images/yonsei.png"],
   },
 
   robots: {
